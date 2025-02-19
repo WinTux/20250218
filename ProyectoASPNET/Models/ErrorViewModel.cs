a2e212c7d93842a86cb0841050857f0e352ad211
WinTux <rusok.software@gmail.com>
2025-02-18 21:17:05 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

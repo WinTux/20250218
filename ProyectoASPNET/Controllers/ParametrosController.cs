733bdfd0c93869ad6f0272f7c76781619128cd2f
WinTux <rusok.software@gmail.com>
2025-02-20 21:19:32 -0400

    Parámetros 1

---
+++ ProyectoASPNET/Controllers/ParametrosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoASPNET.Controllers
+{
+    [Route("parametros")]
+    public class ParametrosController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Ejemplo1/{id}")] // https://localhost:7070/parametros/Ejemplo1/123
+        public IActionResult Ejemplo1(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+    }
+}

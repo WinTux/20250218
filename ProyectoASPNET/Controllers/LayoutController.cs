dd2acd7975f259f380dcba9882c2d776c175f634
WinTux <rusok.software@gmail.com>
2025-02-20 21:08:52 -0400

    Uso de layouts

---
+++ ProyectoASPNET/Controllers/LayoutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoASPNET.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Acerca()
+        {
+            ViewBag.titulo = "ACERCA DE";
+            return View();
+        }
+        public IActionResult Noticias()
+        {
+            return View();
+        }
+    }
+}
--- ProyectoASPNET/Controllers/ValoresDeSettingsController.cs
+++ ProyectoASPNET/Controllers/ValoresDeSettingsController.cs
@@ -2,12 +2,16 @@
 
 namespace ProyectoASPNET.Controllers
 {
+    [Route("propiedades")] // https://localhost:7070/propiedades
     public class ValoresDeSettingsController : Controller
     {
         private IConfiguration _configuration;
         public ValoresDeSettingsController(IConfiguration configuration) { 
             _configuration = configuration;
         }
+        [Route("")] // https://localhost:7070/propiedades
+        [Route("listar")] // https://localhost:7070/propiedades/index
+        [Route("~/")] // https://localhost:7070/propiedades/
         public IActionResult Index()
         {
             ViewBag.valor1 = _configuration["Mensaje"];
@@ -15,6 +19,10 @@ namespace ProyectoASPNET.Controllers
             ViewBag.valor3 = _configuration["MisConfiguraciones:Configuracion2"];
             ViewBag.valor4 = _configuration["MisConfiguraciones:Configuracion3"];
             ViewBag.valor5 = _configuration["Logging:LogLevel:Microsoft.AspNetCore"];
+            return View();
+        }
+        [Route("Diferente")] // https://localhost:7070/propiedades/Diferente
+        public IActionResult Diferente() {
             return View();
         }
     }

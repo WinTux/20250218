a83362a6a9e9367f700eba3d4f4bece7b9e55b45
WinTux <rusok.software@gmail.com>
2025-02-21 21:28:55 -0400

    Parametros 2 & introducción a formularios

--- ProyectoASPNET/Controllers/ParametrosController.cs
+++ ProyectoASPNET/Controllers/ParametrosController.cs
@@ -16,6 +16,33 @@ namespace ProyectoASPNET.Controllers
             ViewBag.Id = id;
             return View();
         }
-
+        [Route("Ejemplo2/{id}/{id2}")] // https://localhost:7070/parametros/Ejemplo2/123/producto07
+        public IActionResult Ejemplo2(int id,string id2)
+        {
+            ViewBag.Id = id;
+            ViewBag.Id2 = id2;
+            return View("DosParametros");
+        }
+        [Route("Ejemplo3")] // https://localhost:7070/parametros/Ejemplo3?prod=producto123
+        public IActionResult Ejemplo3([FromQuery(Name ="prod")] string id)
+        {
+            ViewBag.Id = id;
+            return View("QueryStrings1");
+        }
+        [Route("Ejemplo4")] // https://localhost:7070/parametros/Ejemplo4?idprod=11&prod=producto123
+        public IActionResult Ejemplo4([FromQuery(Name = "prod")] string producto, [FromQuery(Name ="idprod")] int idproducto)
+        {
+            ViewBag.Id = idproducto;
+            ViewBag.Nombre = producto;
+            return View("QueryStrings2");
+        }
+        [Route("Ejemplo5")] // https://localhost:7070/parametros/Ejemplo5?idprod=11&prod=producto123
+        public IActionResult Ejemplo5() {
+            var idproducto = int.Parse(HttpContext.Request.Query["idprod"].ToString());
+            var producto = HttpContext.Request.Query["prod"].ToString();
+            ViewBag.Id = idproducto;
+            ViewBag.Nombre = producto;
+            return View("QueryStrings2");
+        }
     }
 }
---
+++ ProyectoASPNET/Models/Cuenta.cs
@@ -0,0 +1,14 @@
+﻿namespace ProyectoASPNET.Models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public string Ususario { get; set; }
+        public string Password { get;set; }
+        public string Descripcion {  get; set; }
+        public bool Disponible { get; set; }
+        public string Genero { get; set; } // M (Masculino), F (Femenino)
+        public List<string> Lenguajes { get; set; } // l01, l02, etc.
+        public string Cargo { get; set; } // c01, c02
+    }
+}

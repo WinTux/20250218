c75519a4b1e9e44caaa64c1fbaec01472feef26a
WinTux <rusok.software@gmail.com>
2025-03-06 21:22:00 -0400

    Ejemplo QR, inicio

---
+++ ProyectoASPNET/Controllers/CodigoQRController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoASPNET.Controllers
+{
+    public class CodigoQRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GenerarCodigoQR(string productoid)
+        {
+            return View();
+        }
+    }
+}
